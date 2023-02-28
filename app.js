479e58a7d6db7f16cdb63a91d5d1b53652e2aa8e
kittinaphoenix <94007304+kittinaphoenix@users.noreply.github.com>
2023-02-28 08:50:28 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const indexRoutes = require('./routes/index');
+
+const app = express();
+
+const mongoDBSTR = process.env.mongoDBSTR || '';
+
+// Set the strictQuery option to false
+mongoose.set('strictQuery', false);
+
+// Connect to MongoDB
+mongoose.connect(mongoDBSTR, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+db.on('error', (error) => {
+  console.error('MongoDB connection error:', error);
+});
+
+db.once('open', () => {
+  console.log('MongoDB connected!');
+});
+
+db.on('disconnected', () => {
+  console.log('MongoDB disconnected! Trying to reconnect...');
+  mongoose.connect(mongoDBSTR, { useNewUrlParser: true, useUnifiedTopology: true });
+});
+
+// Set up middleware, routes, etc.
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.set('view engine', 'ejs');
+app.use('/', indexRoutes);
+app.use(express.static('public'));
+
+// middleware to handle invalid GET requests
+app.use((req, res, next) => {
+  if (req.method === 'GET') {
+    // render the default error template
+    res.status(404).render('error.ejs');
+  } else {
+    // pass the request to the next middleware
+    next();
+  }
+});
+
+// Start the server
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
